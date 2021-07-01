eb5a49e4fd63488f305659c11c228e9cbae1525d
Dallas Hoffman <dallashoff@gmail.com>
2021-07-01 18:28:04 -0400

    Add types for OpenRGB objects

---
+++ src/rgb.ts
@@ -0,0 +1,38 @@
+// A color (e.g. an RGB or HSL color made up of its 3 channel values)
+export type color = [number, number, number];
+
+// A color object as used by the OpenRGB SDK
+export interface openRgbColor {
+    red: number;
+    green: number;
+    blue: number;
+}
+
+// Describes a device as returned by the OpenRGB SDK
+export interface rgbDevice {
+    deviceId: number;
+    type: number;
+    name: string;
+    description: string;
+    version: string;
+    serial: string;
+    location: string;
+    activeMode: number;
+    leds: [
+        {
+            name: string;
+            value: openRgbColor;
+        }
+    ];
+    colors: openRgbColor[];
+    modes: any[];
+    zones: any[];
+    [key: string]: any;
+}
+
+// Describes a device running the OpenRGB SDK server
+export interface rgbServer {
+    name: string;
+    host: string;
+    port: number;
+}
