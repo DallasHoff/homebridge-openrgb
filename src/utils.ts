1224f5b157f05eda8b58edabd3cc5e5b9bdc0974
Dallas Hoffman <dallashoff@gmail.com>
2022-03-22 23:08:49 -0400

    findDeviceModeId utility function

---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+import { rgbDevice } from './rgb';
+
+/** Finds the ID of a device mode by name or returns undefined if the device has no matching mode */
+export function findDeviceModeId(device: rgbDevice, modeName: string): number | undefined {
+  return device.modes?.find(mode => mode.name?.toLowerCase() === modeName.toLowerCase())?.id;
+}
